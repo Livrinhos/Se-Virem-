3f4d3c51c96a1239367ca081821a5c96be3f44d0
Livrinho <wolfgamer0107@gmail.com>
2026-05-26 16:34:14 -0300

    Validador de Segurança
    Fix function name and add validation for age and animal type.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+functiom filtroEntrada(nome, animal, vip){
+if (nome === ""){
+  return "Erro 400, O nome não pode estar vazio!!";
+}
+if (idade > 20 || idade < 0) {
+  return "Erro 400, Idade Inválida!!";
+}
+if (animal === "Gato" && vip === false){
+  return "Erro 400, Seu Animal, não é um Gato!!";
+}
 
+console.log(filtoEntrada("",10,"Burro",true));
+console.log(filtoEntrada("Toto",42,"Cachorro",false));
+console.log(filtoEntrada("Cat",17,"Gato",true));
+  console.log(filtoEntrada("Cat",17,"Gato",false));
